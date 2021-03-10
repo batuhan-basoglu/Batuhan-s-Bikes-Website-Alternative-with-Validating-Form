8fec5668a0e324f18c492a21baf013b9cd40c47a
soyed <oyediransamuel1995@gmail.com>
2021-03-10 14:14:35 -0500

    modified contact inform form

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+// Calendar Validation
+function toAppointment() {
+  document.getElementById('appointment').scrollIntoView();
+}
+
+var value = 0;
+
+$(function() {
+  $("#datepicker").datepicker({
+    beforeShowDay: getAvailability
+  });
+});
+
+function getMechanic(selectObject) {
+  value = selectObject.value;
+}
+
+function getAvailability(date) {
+
+  if (value == 1 && (date.getDay() === 0 || date.getDay() == 6)) /* restrict weekends */
+    return [false]
+  if (value == 2 && (date.getDay() === 0 || date.getDay() == 1 || date.getDay() == 4 || date.getDay() == 5 || date.getDay() == 6)) /* restrict M,Thurs, Fri, Sat, Sun */
+    return [false]
+  if (value == 3 && (date.getDay() === 0 || date.getDay() == 4 || date.getDay() == 5 || date.getDay() == 6)) /* restrict Thurs, Fri, Sat, Sun */
+    return [false]
+  if (date.getDay() === 0 || date.getDay() == 6) /* weekends */
+    return [false]
+  else
+    return [true, "", ""]
+}
+
+//Form Validation
+function validateForm(){
+  //first check that the user has selected,
+
+}
